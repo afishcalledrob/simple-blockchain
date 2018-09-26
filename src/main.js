2de54c03e50e2bd9aca30c505ecee8514a33adb5
Robert <fishwickrob@live.com>
2018-09-26 15:21:40 +0000

    Added attributes to Transaction class

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 class Transaction{
     constructor(fromAddress, toAddress, amount){
+        this.fromAddress = fromAddress;
+        this.toAddress = toAddress;
+        this.amount = amount;
         
     }
 }
