2f30341adb48a9eb2b30101acc203061bcc85624
Robert <fishwickrob@live.com>
2018-09-26 12:45:58 +0000

    reorganising an error from the last commit, some code was temporarily moved to a different file, now moved back

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
+class Transaction{
+    constructor(fromAddress, toAddress, amount){
+        
+    }
+}
+
 class Block{
-    constructor(index, timestamp, data, previousHash = ''){
-        this.index = index;
+    constructor(timestamp, transactions, previousHash = ''){
         this.timestamp = timestamp;
-        this.data = data;
+        this.transactions = transactions;
         this.previousHash = previousHash;
         this.currentHash;
     }
     
     calculateHash(){
-        this.currentHash = (Math.floor((Math.random() * 9) + 0)).toString() + (Math.floor((Math.random() * 9) + 0)).toString() + (Math.floor((Math.random() * 9) + 0)).toString() + this.index.toString() + this.timestamp + this.data;
+        this.currentHash = (Math.floor((Math.random() * 9) + 0)).toString() + (Math.floor((Math.random() * 9) + 0)).toString() + (Math.floor((Math.random() * 9) + 0)).toString() + this.timestamp + this.transactions;
     }
     
     proofOfWork() {
@@ -24,7 +29,7 @@ class Blockchain{
    }
    
    createGenesisBlock() {
-       var genesisBlock = new Block(0, "01/01/2018", "This is the genesis block", "0");
+       var genesisBlock = new Block("01/01/2018", "This is the genesis block", "0");
        genesisBlock.calculateHash();
        genesisBlock.proofOfWork();
        return genesisBlock;
@@ -34,8 +39,8 @@ class Blockchain{
        return this.chain[this.chain.length - 1];
    }
    
-   addBlock(index, timestamp, data) {
-       var newBlock = new Block(index, timestamp, data, this.getLastBlock().currentHash);
+   addBlock(timestamp, data) {
+       var newBlock = new Block(timestamp, data, this.getLastBlock().currentHash);
        newBlock.calculateHash();
        newBlock.proofOfWork();
        this.chain.push(newBlock);
@@ -45,7 +50,7 @@ class Blockchain{
        for(let i = 1; i < this.chain.length; i++) {
            const currentBlock = this.chain[i];
            const previousBlock = this.chain[i-1];
-           if(currentBlock.index.toString() + currentBlock.timestamp + currentBlock.data !== currentBlock.currentHash.substring(3)) {
+           if(currentBlock.timestamp + currentBlock.transactions !== currentBlock.currentHash.substring(3)) {
                return false;
            }
            if(currentBlock.previousHash !== previousBlock.currentHash) {
